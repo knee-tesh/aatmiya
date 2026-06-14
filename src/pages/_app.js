723955ab9b8405e66ef70536ae01ca37509cbed2
Neha Tiwari <tiwari.neha1115@gmail.com>
2026-06-14 22:34:30 +0530

    feat: add Google Fonts via next/font and final cleanup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
 import '@/styles/globals.css';
-
+import { Playfair_Display, Inter } from 'next/font/google';
 import Layout from '../components/layout';
 
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-display',
+  display: 'swap',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-body',
+  display: 'swap',
+});
+
 export default function App({ Component, pageProps }) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <div className={`${playfair.variable} ${inter.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
